7cab07db2bcce7b4cfeb246cfeeb86f4d9e1eec7
shitou <1348571886@qq.com>
2016-06-16 09:18:06 +0800

    init

--- js/materialDesign.js
+++ js/materialDesign.js
@@ -13,42 +13,68 @@
         for (var i = 0, btn; btn = document.querySelectorAll('[materialDesign]')[i++];) {
             btn.addEventListener('touchstart', materialDesignBg);
             btn.addEventListener('mousedown', materialDesignBg);
+        }
+    }
 
-            function materialDesignBg(e) {
-                e.preventDefault();
-                var tag = this;
-                if (this.getAttribute('materialDesign') === undefined) {
-                    tag = this.parentNode;
-                }
-                var div = tag.querySelector(".animate-hand");
-                if (!div) {
-                    div = document.createElement("div");
-                }
-                div.className = 'animate-hand';
-                tag.insertBefore(div,tag.firstElementChild);
-                var x = e.pageX || e.changedTouches[0].pageX || 0;
-                var y = e.pageY || e.changedTouches[0].pageY || 0;
-                var left = tag.offsetLeft;
-                var top = tag.offsetTop;
-                var width = div.offsetWidth;
-                var height = div.offsetHeight;
-                var scale = Math.round(tag.offsetWidth / 100) || 1;
-                div.style.left = x - left - width / 2 + "px";
-                div.style.top = y - top - height / 2 + "px";
-                scale =scale>6?6:scale;
-                div.className = "animate-hand animate ripple_" + (e.changedTouches ? scale + 1 : scale);
-                if (tag.nodeName != 'A' && tag.getAttribute("href")) {
-                    location.href = tag.getAttribute("href");
-                }
-                return false;
+    function materialDesignBg(e) {
+        var d = this;
+        for (var i in d) {
+            if (/[a-z]+/.test(i) && typeof d[i] == 'number' && d[i] > 0) {
+                console.log(i, d[i]);
             }
         }
+        console.log(e.target);
+        e.preventDefault();
+        // e.stopPropagation(); 
+        var tag = this;
+        if (this.getAttribute('materialDesign') === undefined) {
+            tag = this.parentNode;
+        }
+
+        /*if (e.changedTouches) {
+            var item = e.changedTouches[0];
+            console.log(item.clientX, item.pageX, item.screenX);
+            console.log(item.clientY, item.pageY, item.screenY);
+        }
+        console.log(tag.offsetWidth, tag.clientWidth, tag.scrollWidth);
+        console.log(tag.offsetHeight, tag.clientHeight, tag.scrollHeight);
+        console.log(tag.offsetLeft, tag.clientLeft, tag.scrollLeft);
+        console.log(tag.offsetTop, tag.clientTop, tag.scrollTop);
+*/
+        var div = tag.querySelector(".animate-hand");
+        if (!div) {
+            div = document.createElement("div");
+        }
+        div.className = 'animate-hand';
+        tag.insertBefore(div, tag.firstElementChild);
+
+        var scale = Math.round(tag.offsetWidth / 100) || 1;
+
+        var left = 0;
+        var top = 0;
+        //触摸
+        if (e.changedTouches) {
+            left = e.changedTouches[0].pageX - e.changedTouches[0].target.x;
+            top = e.changedTouches[0].pageY - e.changedTouches[0].target.y;
+        } else {
+            left = e.layerX;
+            top = e.layerY;
+        }
+
+        div.style.left = left + "px";
+        div.style.top = top + "px";
+        scale = scale > 6 ? 6 : scale;
+        div.className = "animate-hand animate ripple_" + (e.changedTouches ? scale + 1 : scale);
+        if (tag.nodeName != 'A' && tag.getAttribute("href")) {
+            location.href = tag.getAttribute("href");
+        }
+        return false;
     }
 })();
 /*模板引擎*/
-var TemplateEngine = function(html, options,reg) {
-	var reg = reg||['<%','%>'];
-	var re = new RegExp(reg[0]+'([^'+reg[1]+']+)?'+reg[1],'g');
+var TemplateEngine = function(html, options, reg) {
+    var reg = reg || ['<%', '%>'];
+    var re = new RegExp(reg[0] + '([^' + reg[1] + ']+)?' + reg[1], 'g');
     var reExp = /(^(\s)?(if|for|else|switch|case|break|{|}))(.*)?/g;
     var code = 'var r=[];\n';
     var cursor = 0;
